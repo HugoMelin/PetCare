63f39795114b9a19f06529fec1b0a761bb423a64
Hugo Melin <hugo.melin@torefi.fr>
2025-12-01 15:18:11 +0100

    Mise en place d'un layout global et navigation

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="bg-white border-b border-gray-200 sticky top-0 z-10">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
+      <NuxtLink to="/">
+        <h1 class="text-3xl text-[#4CA8E0]">PetCare</h1>
+      </NuxtLink>
+    </div>
+  </header>
+</template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="min-h-screen bg-gray-50">
+    <AppHeader />
+    <slot />
+
+    <AppNavigation />
+  </div>
+</template>
