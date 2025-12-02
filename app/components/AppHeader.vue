2617258234180c8aa084d96c8e5c47dc00241052
Hugo Melin <hugo.melin@torefi.fr>
2025-12-02 17:21:14 +0100

    feat(front): added PetCare logo in header and updated app configuration

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,7 +1,14 @@
 <template>
   <header class="bg-white border-b border-gray-200 sticky top-0 z-10">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
-      <NuxtLink to="/">
+      <NuxtLink to="/" class="flex items-center">
+        <NuxtImg
+          src="/images/PetCare_logo.png"
+          alt="Logo PetCare"
+          width="40"
+          height="40"
+          class="inline-block mr-3"
+        />
         <h1 class="text-3xl text-primary">PetCare</h1>
       </NuxtLink>
     </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,18 +2,29 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: [
-    '@nuxt/eslint',
-    '@nuxtjs/tailwindcss',
-    'nuxt-lucide-icons'
-  ],
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', 'nuxt-lucide-icons', '@nuxt/image'],
   css: [
     '~/assets/css/global.css',
   ],
   tailwindcss: {
-    viewer: true, // ouvre un viewer pour explorer les classes disponibles
+    viewer: true,
   },
   lucide: {
     namePrefix: 'Icon'
+  },
+  app: {
+    head: {
+      title: 'PetCare',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: "PetCare - Votre compagnon de confiance pour les services de soins pour animaux." },
+      ],
+      htmlAttrs: {
+        lang: 'fr',
+      },
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' }
+      ]
+    }
   }
 })
