96014b98dbacfc6c19ae58f5a04c1e87c280523e
Hugo Melin <hugo.melin@torefi.fr>
2026-04-29 13:15:54 +0200

    feat: Used resend api as mailer

--- server/api/medications/notifications.get.js
+++ server/api/medications/notifications.get.js
@@ -1,7 +1,29 @@
 import { getAllMedicationsNotifications } from "~~/server/utils/medicationsNotfications";
+import { sendEmail } from "~~/server/utils/resend";
 
 export default defineEventHandler(async () => {
   const result = await getAllMedicationsNotifications();
 
-  return result;
+  const emailHTML =
+    "<p>Congrats on sending your <strong>first email</strong>!</p>";
+
+  try {
+    await sendEmail(
+      "Notifications PetCare <contact@petcare.torefi.fr>",
+      "hugo.melin@torefi.fr",
+      "Hello World",
+      emailHTML,
+    );
+
+    return {
+      result,
+      emailSent: true,
+    };
+  } catch (error) {
+    return {
+      result,
+      emailSent: false,
+      error: error instanceof Error ? error.message : "Erreur inconnue",
+    };
+  }
 });
---
+++ server/utils/resend.ts
@@ -0,0 +1,17 @@
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmail = async (
+  from: string,
+  to: string,
+  subject: string,
+  html: string,
+) => {
+  return await resend.emails.send({
+    from,
+    to,
+    subject,
+    html,
+  });
+};
